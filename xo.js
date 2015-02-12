300230ca41bc359700a49724d11ebaa62fb87940
Gerard Ryan <gerard@ryan.lt>
2015-02-12 13:32:16 +0000

    Switch to O or X depending on player

--- xo.js
+++ xo.js
@@ -1,7 +1,21 @@
 function game() {
 
+    var player = "X"; // first player gets to be X
+
+    // Basic click handler to change to X or O depending on player
     $(".game-cell").click(function() {
-        $(this).text("x");
+        $(this).text(player);
+        player = switch_player(player);
     });
 
+}
+
+// Player changes after each turn
+function switch_player(player) {
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X"
+    }
+    return player;
 }
