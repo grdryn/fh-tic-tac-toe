eb49c5264e8b6f319e0bf89fedda1bf9f56d0cce
Gerard Ryan <gerard@ryan.lt>
2015-02-12 13:47:58 +0000

    Make a function to get the board values

--- xo.js
+++ xo.js
@@ -1,3 +1,10 @@
+var boardIds = new Array(
+        "colarow1", "colbrow1", "colcrow1",
+        "colarow2", "colbrow2", "colcrow2",
+        "colarow3", "colbrow3", "colcrow3"
+);
+
+
 function game() {
 
     var player = "X"; // first player gets to be X
@@ -18,4 +25,13 @@ function switch_player(player) {
         player = "X"
     }
     return player;
+}
+
+//Get the values from the cells IDs (boardIds)
+function getBoardValues() {
+    values = new Array(boardIds.length);
+    for (var index = 0; index < boardIds.length; index++) {
+        values[index] = $("#" + boardIds[index]).text();
+    }
+    return values;
 }
